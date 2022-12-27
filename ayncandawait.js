eabf0ffabfbc7c63a51a5aaa49459d6b8246d216
pavithra147 <pavithra.jayavel@aspiresys.com>
2022-12-27 16:19:29 +0530

    async & await

--- ayncandawait.js
+++ ayncandawait.js
@@ -1,11 +1,20 @@
 let promise =new Promise((resolve,reject)=>{
-    setTimeout(function(){
-        resolve("Hello")},2000)
-    
+    // setTimeout(function(){
+    //     resolve("Hello")},2000)
+    var a=10;
+        if(a>5){
+          setTimeout(()=>{
+             resolve("a is greater")},1000)
+          }
+          else{
+            setTimeout(()=>{
+              reject("a is smaller")},1000)
+          }
+          });
         
       
     
-});
+
 async function example(){
   
   try{
